7b171e8d62b4071106c6c5d714c9b7845eda9152
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-03-06 22:51:49 +0900

    ログマクロの更新: エラーステータスのみをログに記録するように変更

--- dbt_project/macros/log_results.sql
+++ dbt_project/macros/log_results.sql
@@ -3,11 +3,12 @@
   {% if execute %}
   {{ log("========== Begin Summary ==========", info=True) }}
   {% for res in results -%}
-    {% set line -%}
-        node: {{ res.node.unique_id }}; status: {{ res.status }} (message: {{ res.message }})
-    {%- endset %}
-
-    {{ log(line, info=True) }}
+    {% if res.status == 'error' %}
+      {% set line -%}
+          node: {{ res.node.unique_id }}; status: {{ res.status }} (message: {{ res.message }})
+      {%- endset %}
+      {{ log(line, info=True) }}
+    {% endif %}
   {% endfor %}
   {{ log("========== End Summary ==========", info=True) }}
   {% endif %}
