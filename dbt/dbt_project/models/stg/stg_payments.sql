0fa40cbaa8c7e156a8640f67390424e559768c55
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-03-12 21:35:32 +0900

    モデル修正

--- dbt/dbt_project/models/stg/stg_payments.sql
+++ dbt/dbt_project/models/stg/stg_payments.sql
@@ -15,7 +15,6 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
         -- `amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount,
         current_datetime('Asia/Tokyo') as loaded_at_jst
