40a830ded033427f4561f90999cd82e55fcd6d71
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-06-09 09:46:22 +0900

    fixed model

--- dbt/dbt_project/models/dwh/dim_supplies.sql
+++ dbt/dbt_project/models/dwh/dim_supplies.sql
@@ -1,5 +1,7 @@
 select
+    supply_uuid,
     supply_id,
+    product_id,
     supply_name,
     supply_cost,
     is_perishable_supply
--- dbt/dbt_project/models/dwh/fact_orders.sql
+++ dbt/dbt_project/models/dwh/fact_orders.sql
@@ -29,6 +29,19 @@ joined_orders__order_items as (
     inner join aggregated_order_items as order_items
         on orders.order_id = order_items.order_id
 ),
+aggregated_products__supplies as (
+    select
+        products.product_id,
+        products.product_name,
+        products.product_type,
+        products.product_price,
+        sum(supplies.supply_cost) as supply_cost
+    from {{ ref('stg_products') }} as products
+    inner join {{ ref('stg_supplies') }} as supplies
+        on products.product_id = supplies.product_id
+    group by all
+    order by 1
+),
 final as (
     select
         joined_orders__order_items.product_id,
@@ -36,9 +49,10 @@ final as (
         joined_orders__order_items.location_id,
         joined_orders__order_items.ordered_at,
         joined_orders__order_items.order_quantity,
-        products.product_price
+        products_supplies.product_price,
+        joined_orders__order_items.order_id
     from joined_orders__order_items
-    left join {{ ref('stg_products') }} as products
-        on joined_orders__order_items.product_id = products.product_id
+    inner join aggregated_products__supplies as products_supplies
+        on joined_orders__order_items.product_id = products_supplies.product_id
 )
-select * from final
+select count(*) from final
